8ea11efcd9b008cc8e61afe4b9354265f93bd711
Chavez Harris <codedbychavez@gmail>
2022-08-20 22:07:27 -0300

    Completed sample app

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,32 @@
 import 'dart:html';
+// Import the SDK package
+import 'package:configcat_client/configcat_client.dart';
 
-void main() {
-  querySelector('#output')?.text = 'Your Dart app is running.';
+// Create the client
+final client = ConfigCatClient.get(
+    sdkKey: 'C4PaCOYeoEyI73ugq1jJLg/Gg-O0SyO-UKZHwVJOwAc1A');
+
+final HtmlElement thumbnailConverterElement =
+    querySelector('#thumbnailConverter') as HtmlElement;
+
+final HtmlElement featureNotAvailableElement =
+    querySelector('#featureNotAvailable') as HtmlElement;
+
+Future<void> main() async {
+
+  // Check the value of the flag
+  final canShowThumbnailConverter = await client.getValue(
+      key: 'canshowthumbnailconverter', defaultValue: false);
+
+  // If the flag is switched on
+  if (canShowThumbnailConverter) {
+
+    // Show the thumbnailConverterElement
+    thumbnailConverterElement.hidden = false;
+
+  } else {
+
+    // How the featureNotAvailableElement
+    featureNotAvailableElement.hidden = false;
+  }
 }
